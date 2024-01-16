ad3ef2b482223617afcf63a512a0756774173c25
BlancShon <tora15959@gmail.com>
2024-01-16 11:50:30 +0900

    git 초기화 작업 / 2024-01-16 / 이병집
     변경내용
     - git 작업 초기화
     - gitignore 작성
     - 프로젝트 폴더 생성

---
+++ Culture-connector/src/main/java/com/multi/mvc/culture/controller/CultureController.java
@@ -0,0 +1,54 @@
+package com.multi.mvc.culture.controller;
+
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.multi.mvc.api.OpenApiManagerForXml;
+import com.multi.mvc.board.controller.BoardController;
+import com.multi.mvc.board.model.vo.BoardCategory;
+import com.multi.mvc.culture.model.mapper.CultureMapper;
+//import com.multi.mvc.common.util.PageInfo;
+import com.multi.mvc.culture.model.service.CultureService;
+import com.multi.mvc.culture.model.vo.Culture;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class CultureController {
+	
+	@Autowired
+	private CultureService service;
+	
+	List<Culture> list = OpenApiManagerForXml.parse();
+	
+	private static Culture culture;
+	
+	@Bean(initMethod = "init")
+	public void init() {
+		service.save();
+	
+	}
+	
+	@GetMapping("/culture/list")
+	public String list(Model model, Culture culture) {
+		
+		
+		
+		
+		
+		
+		//service.count();
+		
+		model.addAttribute("list", list);
+		return "culture/cultureList";
+	}
+
+}
---
+++ Culture-connector/src/main/java/com/multi/mvc/culture/model/mapper/CultureMapper.java
@@ -0,0 +1,16 @@
+package com.multi.mvc.culture.model.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.multi.mvc.culture.model.vo.Culture;
+
+@Mapper
+public interface CultureMapper {
+	
+	void insertCulture(List<Culture> list);
+	
+	int selectCount();
+
+}
