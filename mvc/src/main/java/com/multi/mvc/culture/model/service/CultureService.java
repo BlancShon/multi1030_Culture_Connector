5eb737cd1793049092325c504c24372c4b91c534
jkk300000 <76241426+jkk300000@users.noreply.github.com>
2024-01-16 13:56:20 +0900

    test jsp 추가

---
+++ mvc/src/main/java/com/multi/mvc/culture/model/service/CultureService.java
@@ -0,0 +1,44 @@
+package com.multi.mvc.culture.model.service;
+
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.multi.mvc.api.OpenApiManagerForXml;
+import com.multi.mvc.culture.model.mapper.CultureMapper;
+import com.multi.mvc.culture.model.vo.Culture;
+
+
+@Service
+public class CultureService {
+
+	
+	
+	@Autowired
+	private  CultureMapper mapper;
+	
+
+	public int save() {
+		int result = 0;
+		
+		
+		mapper.insertCulture(OpenApiManagerForXml.parse());
+		return result;
+		
+		
+		
+		
+		
+	}
+	
+	public int count() {
+		
+		mapper.selectCount();
+		
+		return mapper.selectCount();
+		
+		
+	}
+}
